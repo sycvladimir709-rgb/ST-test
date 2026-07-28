64e526485e2aed9f7a7e257e40e1840b7fb65a17
sycvladimir709-rgb <sycvladimir709@gmail.com>
2026-07-28 11:44:08 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Игры ")
+st.header("Компьютерные")
+st.write("texst")
+st.subheader("Steam")
+st.image("steam.webp")
+st.header("Мобильные")
+st.subheader("AppStor")
+st.image("App Store.webp")
+st.image("1.jpg")
+st.image("2.jpg")
+st.image("3.jpg")
